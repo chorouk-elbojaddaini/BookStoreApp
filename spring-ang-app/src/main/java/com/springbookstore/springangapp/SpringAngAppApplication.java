482d8f416e12d6a05033e402e3e14926134dc9fa
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-02-19 12:04:20 +0100

    starting

---
+++ spring-ang-app/src/main/java/com/springbookstore/springangapp/SpringAngAppApplication.java
@@ -0,0 +1,13 @@
+package com.springbookstore.springangapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngAppApplication.class, args);
+	}
+
+}
---
+++ spring-ang-app/src/test/java/com/springbookstore/springangapp/SpringAngAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbookstore.springangapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
