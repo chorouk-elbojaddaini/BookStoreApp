6a5d2b1249e3c3eccc905cbb9304cb0dbb5ade8a
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-02-25 23:54:26 +0100

    HomePageChanges

--- frontend/book-store-app/src/app/app.module.ts
+++ frontend/book-store-app/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/book-store-app/src/app/home/home.component.ts
+++ frontend/book-store-app/src/app/home/home.component.ts
@@ -1,10 +1,90 @@
 import { Component } from '@angular/core';
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  // items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  // customOptions: OwlOptions = {
+  //   loop: true,
+  //   mouseDrag: true,
+  //   touchDrag: true,
+  //   pullDrag: true,
+  //   dots: false,
+  //   navSpeed: 700,
+  //   navText: ['<i class="fa-solid fa-circle-chevron-left"></i>', '<i class="fa-solid fa-circle-chevron-right"></i>'],
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     400: {
+  //       items: 2
+  //     },
+  //     740: {
+  //       items: 3
+  //     },
+  //     940: {
+  //       items: 4
+  //     }
+  //   },
+  //   nav: true
+  // }
+  slides = [
+    {
+      title: 'Slide 1',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 1'
+    },
+    {
+      title: 'Slide 2',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 2'
+    },
+    {
+      title: 'Slide 3',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 3'
+    },
+    {
+      title: 'Slide 4',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 4'
+    },
+    {
+      title: 'Slide 5',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 5'
+    },
+    {
+      title: 'Slide 6',
+      image: '../../assets/images/library1.jpg',
+      alt: 'Slide 6'
+    }
+  ];
 
+  customOptions = {
+    loop: true,
+    margin:10,
+    nav: true,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    rewind: false, // disable rewind
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      768: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3
+      }
+    }
+  };
 }
+
+
